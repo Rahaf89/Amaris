5fb8e226104c6507e2ddbcef57dbad2a3ea3dd2b
rahaf89 <rahaflh.900@hotmail.com>
2020-06-08 19:10:46 +0200

    poicies endpoint

--- Handlers/policies.js
+++ Handlers/policies.js
@@ -1,15 +1,13 @@
-app.get("/policies", (req, res) => {
+const db = require('./Models/DB_Config'); 
+
+
+app.get("/policies/clientname", (req, res) => {
   
     pool
-      .query("SELECT clients.*, policies.id FROM clients join policies on clients.id = policies.clientid")
+      .query("SELECT policies.*, name FROM clients join policies on clients.id = policies.clientid")
       .then(result => res.json(result.rows))
       .catch(err => res.json(err, 500));
   });
 
 
-  app.get("/policies/name", (req, res) => {
-    pool
-      .query("SELECT policies.* , clients.name FROM clients join policies on clients.id = policies.clientid")
-      .then((result) => res.json(result.rows))
-      .catch((err) => res.json(err, 404));
-  });
+  
