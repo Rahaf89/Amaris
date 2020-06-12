3e509ebd84aad85d79f31488cbf9400b68b25b9c
rahaf89 <rahaflh.900@hotmail.com>
2020-06-12 02:17:33 +0200

    spaces

--- App.js
+++ App.js
@@ -5,16 +5,13 @@ const usersRouter = require('./Handlers/clients');
 const policiesRouter = require('./Handlers/policies');
 const router = express.Router();
 
-
 app.route("/users", usersRouter);
 app.route("/policies", policiesRouter)
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 
-
 module.exports = router, app;
-
 
 app.listen(3000, function() {
     console.log("Server is listening on port 3000. Ready to accept requests!");
