ef7dc76bc8b992527456308af2122754c7786a1c
rahaf89 <rahaflh.900@hotmail.com>
2020-06-14 11:08:05 +0200

    correction

--- auth.js
+++ auth.js
@@ -1,23 +1,21 @@
 var jwt = require('jwt-simple')
-var secrets = require('./secrets')
 var router = require('express').Router()
-var bcrypt = require('bcrypt')
 var jwt    = require('jwt')
 var User   = require('./models/dbConfig')
 
 
-router.get('/users', function (req, res, next) {
+router.get('/clients', function (req, res, next) {
   if (!req.headers['x-auth']) {
     return res.sendStatus(401)
   }
-  var auth = jwt.decode(req.headers['x-auth'])
+  var auth = jwt(req.headers['x-auth'])
   User.findOne({username: auth.username}, function (err, user) {
     if (err) { return next(err) }
     res.json(user)
   })
 })
 
-router.post('/users', function (req, res, next) {
+router.post('/clients', function (req, res, next) {
   var user = new User({username: req.body.username})
     if (err) { return next(err) }
     user.save(function (err) {
@@ -30,7 +28,6 @@ module.exports = router
 
 module.exports = function (req, res, next) {
   if (req.headers['x-auth']) {
-    req.auth = jwt.decode(req.headers['x-auth'])
+    req.auth = jwt(req.headers['x-auth'])
   }
-  next()
 }
