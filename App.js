034dd653784179e3224ec26ef93cb04a77fe84ef
rahaf89 <rahaflh.900@hotmail.com>
2020-06-16 14:15:33 +0200

    users route

--- App.js
+++ App.js
@@ -23,7 +23,7 @@ app.use(authMiddleware(pool));
 
 
 app.use("/users", usersRouter(pool));
-app.use("/policies", policiesRouter(pool));
+app.use("/policies", usersRouter(pool));
 
 
 app.listen(3000, function() {
