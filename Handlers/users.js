b39bd63f53a3b83b871768e7ac1092526f8f5e46
rahaf89 <rahaflh.900@hotmail.com>
2020-06-19 00:32:33 +0200

    readme

--- Handlers/users.js
+++ Handlers/users.js
@@ -5,12 +5,12 @@ function usersRouter(db) {
   const router = express.Router();
 
   // get all the users
-  router.get("/", checkAdminMiddleware, (req, res) => {
+  router.get("/", checkAdminMiddleware, (req, res, next) => {
     db.query(
       "SELECT clients.* FROM clients join policies on clients.id = policies.clientid",
       (error, results) => {
         if (error) {
-          throw error;
+          return next(error);
         }
         res.status(200).json(results.rows);
       }
@@ -18,19 +18,19 @@ function usersRouter(db) {
   });
 
   // get the user filtered by id
-  router.get("/id/:id", (req, res) => {
+  router.get("/id/:id", checkAdminMiddleware, (req, res, next) => {
     const { id } = req.params;
 
     db.query("SELECT * FROM clients WHERE id = $1", [id], (error, results) => {
       if (error) {
-        throw error;
+        return next(error);
       }
       res.status(200).json(results.rows);
     });
   });
 
   // get the user filtered by name
-  router.get("/name/:name", (req, res) => {
+  router.get("/name/:name", checkAdminMiddleware, (req, res, next) => {
     const { name } = req.params;
 
     db.query(
@@ -38,7 +38,21 @@ function usersRouter(db) {
       [`%${name}%`],
       (error, results) => {
         if (error) {
-          throw error;
+          return next(error);
+        }
+        res.status(200).json(results.rows);
+      }
+    );
+  });
+  router.get("/policy/:id", checkAdminMiddleware, (req, res, next) => {
+    const { id } = req.params;
+
+    db.query(
+      "SELECT clients.*, policies.id FROM clients join policies on clients.id = policies.clientid where policies.id = $1",
+      [id],
+      (error, results) => {
+        if (error) {
+          return next(error);
         }
         res.status(200).json(results.rows);
       }
